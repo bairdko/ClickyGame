dc600b0240172d87c841e7d1bf8bbb500d93cc00
bairdko <bairdko@gmail.com>
2018-11-10 10:22:34 -0600

    shuffle works

--- src/App.js
+++ src/App.js
@@ -17,16 +17,61 @@ const picStyle = {
 
 class App extends Component {
 
-  state = {
-    score: 0,
-    topScore: 0,
-    pokemon: []
+  constructor(props){
+    super(props)
+    this.state = {
+      score: 0,
+      topScore: 0,
+      pokemon: []
+    }
+
+    this.imageClicked = this.imageClicked.bind(this)
   }
+
+  
 
   componentDidMount(){
 
-    this.setState({pokemon: pokemon})
+    this.setState({pokemon: pokemon});
 
+  }
+
+  //this doesnt work yet
+  imageClicked(e){
+    this.shuffleArray(this.state.pokemon);
+
+    let score = 0;
+
+    if(!e.target.value){
+      score += 1;
+      e.target.value = false;
+
+      this.setState({score: score},function(){
+        if(this.state.score > this.state.topScore){
+          this.setState({topScore: this.state.score})
+        }
+      });
+    }
+    else {
+      this.setState({ score: 0})
+    }
+
+  }
+
+  //scoring doesn't work yet
+  shuffleArray(array) {
+
+      let j, x, i;
+      
+      for (i = array.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = array[i];
+        array[i] = array[j];
+        array[j] = x;
+      }
+      
+      this.setState({pokemon: array})
+    
   }
 
   render() {
@@ -39,8 +84,8 @@ class App extends Component {
           <Row><br/></Row>
           <Row>
             {this.state.pokemon.map( pokemon => (
-              <Col md="3" sm="4" xs="6">
-                <img alt = {pokemon.name} src = {pokemon.image} style = {picStyle} value = {pokemon.clicked}/>
+              <Col md="3" sm="4" xs="6" key = {pokemon._id}>
+                <img alt = {pokemon.name} src = {pokemon.image} style = {picStyle} value = {pokemon.clicked} onClick={this.imageClicked}/>
               </Col>
             ))}
           </Row>
